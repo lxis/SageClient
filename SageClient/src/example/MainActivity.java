fd3ab425e3b95b1b1fb93d492c3275cc4f89e327
lxis <lxis@163.com>
2014-07-28 22:33:00 +0800

    + async helper

--- SageClient/src/example/MainActivity.java
+++ SageClient/src/example/MainActivity.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import org.apache.http.message.BasicNameValuePair;
 
+import com.sage.client.AsyncHelper;
 import com.sage.client.RestClient;
 import com.sage.client.RestRequest;
 import com.sageclient.example.R;
@@ -14,32 +15,41 @@ import android.view.Menu;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        loadDataFromNet();
-    }
-
-
-    private void loadDataFromNet() {
-		int lastIndex = 1;
-		int singleCount = 1;
-    	
-    	RestRequest request = new RestRequest();
-		request.Url = "http://182.92.4.28/articlehandler.ashx";		
-		request.GetParamDic.add(new BasicNameValuePair("in",Integer.toString(lastIndex)));
-		request.PostParamDic.add(new BasicNameValuePair("rn",Integer.toString(singleCount)));		
-		UserDTO jsonObject = RestClient.GetDtoFromServer(request, UserDTO.class);		
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		loadDataFromNet();
 	}
-    
 
+	private void loadDataFromNet() {
+		final int lastIndex = 1;
+		final int singleCount = 1;
+
+		new AsyncHelper<UserDTO>() {
+			@Override
+			protected UserDTO await() {
+				RestRequest request = new RestRequest();
+				request.Url = "http://182.92.4.28/articlehandler.ashx";
+				request.GetParamDic.add(new BasicNameValuePair("in", Integer
+						.toString(lastIndex)));
+				request.PostParamDic.add(new BasicNameValuePair("rn", Integer
+						.toString(singleCount)));
+				return RestClient.GetDtoFromServer(request, UserDTO.class);
+			}
+
+			@Override
+			protected void async(UserDTO result) {
+
+			}
+		};
+	}
 
 	@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
 }
